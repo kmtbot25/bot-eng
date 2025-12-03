28c2b09c10839ebb634459fd0e7335ba3ec2cacf
kmtbot25 <kmtbot05@gmail.com>
2025-12-03 11:22:40 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,32 +39,25 @@ def get_pr_markup():
         types.KeyboardButton('ПР 3'),
         types.KeyboardButton('ПР 4'),
         types.KeyboardButton('ПР 5'),
-        types.KeyboardButton('ПР 6'),
-        types.KeyboardButton('↩️ Назад в меню')
+        types.KeyboardButton('ПР 6')
     )
     return markup
 
-# Клавиатура для выбора тем
-def get_tema_markup():
-    markup = types.InlineKeyboardMarkup()
-    topics = [
-        ("Тема 1. Системы счисления", "tema_1"),
-        ("Тема 2. Алгебра логики", "tema_2"),
-        ("Тема 3. Интернет", "tema_3"),
-        ("Тема 4. Защита информации", "tema_4"),
-        ("Тема 5. Текстовый процессор", "tema_5"),
-        ("Тема 6. Компьютерная графика", "tema_6")
-    ]
-    for text, callback in topics:
-        markup.add(types.InlineKeyboardButton(text, callback_data=callback))
-    return markup
-
 # === ОБРАБОТЧИКИ TELEGRAM ===
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(
+        message.chat.id,
+        "Привет! Я бот для помощи с занятиями. Выберите опцию:",
+        reply_markup=main_markup
+    )
+
 @bot.message_handler(func=lambda message: True)
 def handle_all_messages(message):
     try:
         user_text = message.text
 
+        # Обработка кнопки "Скачать учебник"
         if user_text == 'Скачать учебник':
             bot.send_message(
                 message.chat.id,
@@ -72,13 +65,15 @@ def handle_all_messages(message):
                 reply_markup=main_markup
             )
 
+        # Обработка кнопки "Практические занятия"
         elif user_text == 'Практические занятия':
             bot.send_message(
                 message.chat.id,
                 "Выберите номер практической работы (ПР):",
-                reply_markup=get_pr_markup()
+                reply_markup=get_pr_markup()  # Показываем клавиатуру с ПР 1-6
             )
 
+        # Обработка кнопки "Итоговый тест"
         elif user_text == 'Итоговый тест':
             bot.send_message(
                 message.chat.id,
@@ -86,24 +81,26 @@ def handle_all_messages(message):
                 reply_markup=main_markup
             )
 
+        # Обработка кнопки "Пройти тесты"
         elif user_text == 'Пройти тесты':
             bot.send_message(
                 message.chat.id,
-                "Данный раздел в разработке. Скоро все появится!",
+                "В разработке. Скоро все появится",
                 reply_markup=main_markup
             )
 
-        # Обработка кнопок с практическими работами
+        # Обработка кнопок с практическими работами (ПР 1-6)
         elif user_text.startswith('ПР '):
-            pr_number = user_text.split()[1]  # Получаем номер ПР
+            pr_number = user_text.split()[1]  # Получаем номер ПР (1, 2, 3, 4, 5, 6)
             
+            # Информация о каждой практической работе
             pr_info = {
-                '1': "ПР 1: Системы счисления\nЗадание: Реши самостоятельную в тетради\nСсылка: https://disk.yandex.ru/d/APQE4mDwBTSbkA",
-                '2': "ПР 2: Алгебра логики\nЗадание: Реши задачи из учебника\nСсылка: https://disk.yandex.ru/d/BPjzUvFeiSOvVw",
-                '3': "ПР 3: Интернет\nЗадание: Используй MS Visio или Draw.io\nСсылка: https://disk.yandex.ru/d/w_85PUK6rneizQ",
-                '4': "ПР 4: Защита информации\nЗадание: Используй MS Word\nСсылка: https://disk.yandex.ru/d/JycaZ67-mUxadQ",
-                '5': "ПР 5: Текстовый процессор\nЗадание: Практические работы №9-12\nСсылка: https://disk.yandex.ru/d/aiykc237nTqJBg",
-                '6': "ПР 6: Компьютерная графика\nЗадание: Используй MS Publisher\nСсылка: https://disk.yandex.ru/d/qKQ3ZFQHg59wGQ"
+                '1': "ПР 1: Системы счисления\n\nЗадание: Реши самостоятельную работу в тетради\nРезультат покажи учителю\n\nМатериалы: https://disk.yandex.ru/d/APQE4mDwBTSbkA",
+                '2': "ПР 2: Алгебра логики\n\nЗадание:\n• с.3 Вариант 4, Задачи №1,2\n• с.4 Вариант 3, Задача №1\n• с.11 Вариант 4, Задача №2\n\nРезультат покажи учителю\n\nМатериалы: https://disk.yandex.ru/d/BPjzUvFeiSOvVw",
+                '3': "ПР 3: Интернет\n\nЗадание: Используй MS Visio или Draw.io\n\nРезультат отправь на почту: polinavladimirovn@yandex.ru\n\nМатериалы: https://disk.yandex.ru/d/w_85PUK6rneizQ",
+                '4': "ПР 4: Защита информации\n\nЗадание: Используй MS Word\n\nРезультат отправь на почту: polinavladimirovn@yandex.ru\n\nМатериалы: https://disk.yandex.ru/d/JycaZ67-mUxadQ",
+                '5': "ПР 5: Текстовый процессор\n\nЗадания из учебника:\n• с.31 Практическая работа №9\n• с.37 Практическая работа №10\n• с.41 Практическая работа №11\n• с.46 Практическая работа №12\n\nРезультат отправь на почту: polinavladimirovn@yandex.ru\n\nМатериалы: https://disk.yandex.ru/d/aiykc237nTqJBg",
+                '6': "ПР 6: Компьютерная графика\n\nЗадание: Используй MS Publisher или LibreOffice Impress\n\nРезультат отправь на почту: polinavladimirovn@yandex.ru\n\nМатериалы: https://disk.yandex.ru/d/qKQ3ZFQHg59wGQ"
             }
             
             if pr_number in pr_info:
@@ -119,6 +116,7 @@ def handle_all_messages(message):
                     reply_markup=get_pr_markup()
                 )
 
+        # Обработка кнопки "Назад в меню"
         elif user_text == '↩️ Назад в меню':
             bot.send_message(
                 message.chat.id,
@@ -136,88 +134,6 @@ def handle_all_messages(message):
     except Exception as e:
         bot.send_message(message.chat.id, "Произошла ошибка. Попробуйте ещё раз.")
         print(f"[ERROR] {e}")
-
-# === ОБРАБОТЧИКИ CALLBACK ===
-@bot.callback_query_handler(func=lambda call: call.data.startswith('dz_'))
-def handle_dz_callback(call):
-    dz_number = call.data[3:]
-    assignments = {
-        '1': (
-            "ДЗ 1: Системы счисления\n"
-            "Задание: Реши самостоятельную в тетради, а результат покажи учителю.\n"
-            "Вариант узнай у меня\n"
-            "Ссылка на материалы: https://disk.yandex.ru/d/APQE4mDwBTSbkA"
-        ),
-        '2': (
-            "ДЗ 2: Алгебра логики\n"
-            "Задание: Реши самостоятельно по учебнику, а решение покажи учителю:\n"
-            "с. 3 Вариант 4, Задача №1,2\n"
-            "с. 4 Вариант 3, Задача №1\n"
-            "с.11 Вариант 4, Задача 2.\n"
-            "Ссылка на материалы: https://disk.yandex.ru/d/BPjzUvFeiSOvVw"
-        ),
-        '3': (
-            "ДЗ 3: Интернет\n"
-            "Задание: Для этого задания используй MS Visio или Draw.io\n"
-            "Результат выполнения отправь преподавателю на почту polinavladimirovn@yandex.ru\n"
-            "Ссылка на материалы: https://disk.yandex.ru/d/w_85PUK6rneizQ"
-        ),
-        '4': (
-            "ДЗ 4: Защита информации\n"
-            "Задание: Для этого задания используй MS Word\n"
-            "Результат выполнения отправь преподавателю на почту polinavladimirovn@yandex.ru\n"
-            "Ссылка на материалы: https://disk.yandex.ru/d/JycaZ67-mUxadQ"
-        ),
-        '5': (
-            "ДЗ 5: Текстовый процессор\n"
-            "Задание: Для этого задания используй MS Word\n"
-            "с. 31 Практическая работа №9\n"
-            "с. 37 Практическая работа №10\n"
-            "с. 41 Практическая работа №11\n"
-            "с. 46 Практическая работа №12\n"
-            "Результат выполнения отправь преподавателю на почту polinavladimirovn@yandex.ru\n"
-            "Ссылка на материалы: https://disk.yandex.ru/d/aiykc237nTqJBg"
-        ),
-        '6': (
-            "ДЗ 6: Компьютерная графика\n"
-            "Задание: Для этого задания используй MS Publisher или LO Impress\n"
-            "Результат выполнения отправь преподавателю на почту polinavladimirovn@yandex.ru\n"
-            "Ссылка на материалы: https://disk.yandex.ru/d/qKQ3ZFQHg59wGQ"
-        )
-    }
-
-    if dz_number in assignments:
-        bot.send_message(
-            call.message.chat.id,
-            assignments[dz_number]
-        )
-    else:
-        bot.send_message(call.message.chat.id, "ДЗ не найдено. Обратитесь к преподавателю.")
-
-    bot.answer_callback_query(call.id)
-
-@bot.callback_query_handler(func=lambda call: call.data.startswith('tema_'))
-def handle_tema_callback(call):
-    tema_number = call.data[5:]
-    topics = {
-        '1': "Системы счисления",
-        '2': "Алгебра логики",
-        '3': "Интернет",
-        '4': "Защита информации",
-        '5': "Текстовый процессор",
-        '6': "Компьютерная графика"
-    }
-
-    if tema_number in topics:
-        link = f"https://disk.yandex.ru/d/{['APQE4mDwBTSbkA', 'BPjzUvFeiSOvVw', 'w_85PUK6rneizQ', 'JycaZ67-mUxadQ', 'aiykc237nTqJBg', 'qKQ3ZFQHg59wGQ'][int(tema_number)-1]}"
-        bot.send_message(
-            call.message.chat.id,
-            f"Тема {tema_number}: {topics[tema_number]}. Задание: Напиши конспект Лекции по ссылке в тетрадь. Не забудь про ДЗ! {link}"
-        )
-    else:
-        bot.send_message(call.message.chat.id, "Тема не найдена.")
-
-    bot.answer_callback_query(call.id)
 
 # === WEBHOOK FOR TELEGRAM (OPTIONAL) ===
 @app.route('/webhook', methods=['POST'])
